fb9ecba56874029a7191306d619e0f098855cc63
YaroslavKut <kutilin.y@gmail.com>
2022-08-11 18:19:47 +0300

    Add main index file

--- max.js
+++ max.js
@@ -1,10 +1,10 @@
 number1 = parseFloat(prompt('Enter number1'));
 number2 = parseFloat(prompt('Enter number2'));
 number3 = parseFloat(prompt('Enter number3'));
-if ((number1 > number2) && (number1 > number3)) {
+if ((number1 >= number2) && (number1 >= number3)) {
 alert ('Max number is: '+number1);
 }
-else if ((number2 > number1) && (number2 > number3)) {
+else if ((number2 >= number1) && (number2 >= number3)) {
 alert ('Max number is: '+number2);
 }
 else alert ('Max number is: '+number3);
